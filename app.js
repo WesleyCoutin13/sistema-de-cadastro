0fe28fc690ed85fcd7cd5e1e4c39ec14c45cd428
Wesley Coutinho Correa da Silva <wesleycs@me.local>
2023-02-27 21:26:09 -0300

    Estou voltando a versao do codigo

--- app.js
+++ app.js
@@ -3,4 +3,3 @@ console.log("A aplicação esta conectada ao banco de dados");
 // mais um console log de teste:
 console.log("Fiz um commit de teste");
 console.log("Enviando para o GitHub");
-console.log("console de teste");
