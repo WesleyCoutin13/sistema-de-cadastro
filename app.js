608b1042c25eae8fa2abf62dd15d1128ba1be7ac
Wesley Coutinho Correa da Silva <wesleycs@me.local>
2023-02-27 21:14:12 -0300

    Inserindo uma modificação de teste

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
-console.log("Rodando o sistema de gerenciamento de jogos")
+console.log("Rodando o sistema de gerenciamento de jogos");
+console.log("A aplicação esta conectada ao banco de dados");
+// mais um console log de teste:
+console.log("Fiz um commit de teste");
+console.log("Enviando para o GitHub");
